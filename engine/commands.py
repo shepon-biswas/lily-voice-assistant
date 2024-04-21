64b70046addfd43aa85dabe74e553fc2a595420c
Shepon Biswas <sheponbiswas01@gmail.com>
2024-04-21 18:05:19 +0800

    Install SpeechRecognition and pyAudio and setup a takeCommand function

--- engine/commands.py
+++ engine/commands.py
@@ -1,4 +1,6 @@
 import pyttsx3
+import speech_recognition as spchr
+
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -9,4 +11,23 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("I love python")
+def takeCommand():
+    recog = spchr.Recognizer()
+    with spchr.Microphone() as source:
+        print('Listening...')
+        recog.pause_threshold = 1
+        recog.adjust_for_ambient_noise(source)
+
+        audio = recog.listen(source, 10, 6)
+    try:
+        print('Recognizing...')
+        query = recog.recognize_google(audio, language='en-us')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    return query.lower()
+
+
+text = takeCommand()
+
+speak(text)
