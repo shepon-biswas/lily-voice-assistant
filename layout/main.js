c8e04177428b08560fd89911251c42f72cda5e4f
Shepon Biswas <sheponbiswas01@gmail.com>
2024-04-21 14:43:51 +0800

    Siri Wave Added

--- layout/main.js
+++ layout/main.js
@@ -10,4 +10,30 @@ $(document).ready(function () {
         },
 
     });
+
+    // Siri Wave Script
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 640,
+        height: 200,
+        style: "ios9",
+        amplitude: 1,
+        speed: 0.3,
+        autostart: true
+      });
+
+    // Siri message animation
+    $('.siri-message').textillate({
+        // loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+
+    });
 })
