6fe22ee0ceba327a2568aa6629bc3054827b0b0e
Shepon Biswas <sheponbiswas01@gmail.com>
2024-04-28 13:02:27 +0800

    Create app_name function to open a sys or web command from sql database

--- engine/database.py
+++ engine/database.py
@@ -7,13 +7,17 @@ cursor = connection.cursor()
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
 
-# query = "INSERT INTO sys_command VALUES (null, 'Microsoft excel', 'C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE')"
+# query = "INSERT INTO sys_command VALUES (null, 'Microsoft Excel', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.EXE')"
 # cursor.execute(query)
 # connection.commit()
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
-
-query = "INSERT INTO web_command VALUES (null, 'wikipedia', 'https://www.wikipedia.org/')"
+query = "DELETE FROM sys_command WHERE id=3;"
 cursor.execute(query)
 connection.commit()
+
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null, 'wikipedia', 'https://www.wikipedia.org/')"
+# cursor.execute(query)
+# connection.commit()
--- engine/features.py
+++ engine/features.py
@@ -1,21 +1,58 @@
 import os
 import re
+import sqlite3
+import webbrowser
 from engine.commands import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit
 
+# Import Database
+connection = sqlite3.connect("lily.db")
+cursor = connection.cursor()
 
 def openCommand(query):
     query = query.replace(ASSISTANT_NAME, "")
     query = query.replace("open", "")
     query.lower()
 
-    if query!= "":
-        speak("opening" +query)
-        os.system("start" +query)
+    app_name = query.strip()
 
-    else:
-        speak("Command not found!")
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
+
+
+    # if query!= "":
+    #     speak("opening" +query)
+    #     os.system("start" +query)
+
+    # else:
+    #     speak("Command not found!")
 
 def playYoutube(query):
     searchTerm = extractYtTerm(query)
